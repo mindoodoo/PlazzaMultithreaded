8483590102a18b6de0f0c34af4f21703ef7ff656
Leon Sautour <leon2@sautour.net>
2022-05-12 11:56:07 +0200

    Added SplitString object

---
+++ Kitchen/SplitString.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2022
+** theplazza
+** File description:
+** SplitString
+*/
+
+#include <vector>
+#include <string>
+
+class SplitString {
+public:
+    // Builds new SplitString object
+    // Splits string originalStr on sep
+    // Split string is then available in vector _tokens
+    SplitString(std::string originalStr, std::string sep);
+
+    std::vector<std::string> _tokens;
+
+    const std::string &getOriginalStr() const;
+
+private:
+    std::string const _originalStr;
+};
