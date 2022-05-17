4865b008031468dedbc2b786e8f3afd49e971f2a
Léon Sautour <leon2@sautour.net>
2022-05-17 17:04:43 +0200

    ThreadEncapsulation WIP

---
+++ Kitchen/ThreadEncapsulation.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** ThreadEncapsulation
+** File description:
+** .
+*/
+
+#pragma once
+
+class ThreadEncapsulation {
+public:
+    ThreadEncapsulation(int id);
+
+private:
+    int _id;
+};
